d5e902300c8b2e173a6fa2448423c7e177a47fb3
seanswanson <sean.swanson@dreambox.com>
2021-02-04 11:07:21 -0800

    Added weatherService which contains a method that makes the HTTP request once we select the city name and is passed to the weatherReportComponent. Now rendering some basic wather info.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,24 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  cities = ["London", "Paris", "Moscow", "New York", "Karachi", "Sydney"];
+  cities: String[] = ['London', 'Paris', 'Moscow', 'New York', 'Karachi', 'Sydney'];
   cityControl: FormControl;
 
   constructor(private router: Router, private route: ActivatedRoute) {
   }
+
   ngOnInit() {
     console.log('I\'m initialized!');
+
     this.cityControl = new FormControl("");
+
+    // Set to null as a workaround to display the input's placeholder.
+    // This addresses a bug in the ANTD NG select component.
+    this.cityControl.setValue(null);
+
     this.cityControl.valueChanges.subscribe(value => {
-      console.log(value);
+      console.log('You selected ' + value);
       value ? this.router.navigate([value]) : this.router.navigate(['']);
-    })
+    });
   }
 }
--- src/app/weather-report/weather-report.component.ts
+++ src/app/weather-report/weather-report.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import {map, filter, concatMap } from 'rxjs/operators';
+import {WeatherService} from './weather.service';
 
 @Component({
   selector: 'app-weather-report',
@@ -7,14 +10,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./weather-report.component.scss']
 })
 export class WeatherReportComponent implements OnInit {
+  data$: Observable<any>;
   locationName: string;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private weatherService: WeatherService) { }
 
   ngOnInit() {
     console.log(this.route)
-    this.route.paramMap.subscribe(params => {
-        this.locationName = params.get('locationName');
-      })
+
+    this.data$ = this.route.params.pipe(
+      map(params => params['locationName']),
+      filter(name => !!name),
+      concatMap(name => this.weatherService.getWeatherForCity(name))
+    );
   }
 
 }
